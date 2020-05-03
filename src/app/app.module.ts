c37fb31364818913fe6acbd727ea5d086faf87b9
Alex Merecka <alex.merecka@umbragestudios.com>
2020-05-03 13:19:05 -0500

    Setup navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,8 @@ import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { EditShoppingListComponent } from "./shopping-list/edit-shopping-list/edit-shopping-list.component";
 import { RecipesComponent } from "./recipes/recipes.component";
 import { RecipeListComponent } from "./recipes/recipe-list/recipe-list.component";
-import { RecipeItemComponent } from "./recipes/recipe-item/recipe-item.component";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
+import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-item.component";
 
 @NgModule({
   declarations: [
