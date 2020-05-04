4f3e8c5a70076467b2d156f0be0b9825e7ab7ed8
Alex Merecka <alex.merecka@umbragestudios.com>
2020-05-03 20:25:50 -0500

    Setup recipe item card format

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Recipe } from "./../recipe.model";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      "A Test Recipe",
+      "This is a test",
+      "https://pinchofyum.com/wp-content/uploads/Lo-Mein-1-2.jpg"
+    ),
+    new Recipe(
+      "A Test Recipe",
+      "This is a test",
+      "https://pinchofyum.com/wp-content/uploads/Lo-Mein-1-2.jpg"
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
